696cdbe3f51503cee95ab8ef747f04c7f61d6313
Abdalla Elhagar <nanoelhgar@gmail.com>
2025-07-22 19:51:15 +0300

    Change SignUpSection heading text color from white to black for improved visibility

--- src/components/SignUpSection.tsx
+++ src/components/SignUpSection.tsx
@@ -5,7 +5,7 @@ function SignUpSection() {
   return (
     <section className='py-20 bg-[#ff6600]'>
         <div className="container centralize-content flex-col gap-6 text-center">
-            <h2 className='text-3xl text-white font-bold'>Let ThinkPal Guide Your Learning Journey</h2>
+            <h2 className='text-3xl text-black font-bold'>Let ThinkPal Guide Your Learning Journey</h2>
             <MyLink text='Create Your Free Account' styles='' bgColor='var(--main-color)' textColor='black' href="sign-up" />
         </div>
     </section>
